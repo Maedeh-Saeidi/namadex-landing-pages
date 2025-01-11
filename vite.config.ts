7836802167b0969ebef664e6e8210526c7b5bb92
maedeh <saeidimdhw@gmail.com>
2025-01-11 13:21:56 +0330

    debug

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vitePluginPrettier from "vite-plugin-prettier";
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/",
   plugins: [react(), vitePluginPrettier()],
   server: {
     host: true,
