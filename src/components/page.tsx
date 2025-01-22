8dbb1ff2fb6abbd4f26115ac0411547a5c3135a3
maedeh <saeidimdhw@gmail.com>
2025-01-22 18:37:57 +0330

    update

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,38 +1,38 @@
-import { useState } from "react";
-import { useData } from "../context/DataContext";
-import { Button, Flex, Image, Text } from "@chakra-ui/react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper/modules";
+import { useState } from 'react'
+import { useData } from '../context/DataContext'
+import { Button, Flex, Image, Text } from '@chakra-ui/react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Navigation } from 'swiper/modules'
 //@ts-expect-error some error
-import "swiper/css";
+import 'swiper/css'
 //@ts-expect-error some error
-import "swiper/css/pagination";
+import 'swiper/css/pagination'
 //@ts-expect-error some error
-import "swiper/css/navigation";
-import { IPage } from "../types";
-import { API_URL } from "../api/CONSTANTS";
+import 'swiper/css/navigation'
+import { IPage } from '../types'
+import { API_URL } from '../api/CONSTANTS'
 
 export default function Page({
   page,
   onClose,
 }: {
-  page?: IPage;
-  onClose: () => void;
+  page?: IPage
+  onClose: () => void
 }) {
-  const { data } = useData();
-  const [isFullScreen, setIsFullScreen] = useState(false);
-  const [fullScreenImage, setFullScreenImage] = useState("");
+  const { data } = useData()
+  const [isFullScreen, setIsFullScreen] = useState(false)
+  const [fullScreenImage, setFullScreenImage] = useState('')
 
   const handleImageClick = (img: string) => {
-    setIsFullScreen(!isFullScreen);
-    setFullScreenImage(img);
-  };
+    setIsFullScreen(!isFullScreen)
+    setFullScreenImage(img)
+  }
 
   return (
-    <Flex flexDir={"column"}>
+    <Flex flexDir={'column'}>
       <Flex backgroundColor={data?.[0].jsonColor.primaryColor}>
         <Swiper
-          effect={"coverflow"}
+          effect={'coverflow'}
           spaceBetween={30}
           navigation={true}
           pagination={{
@@ -44,16 +44,16 @@ export default function Page({
           {page?.images.map((img) => (
             <SwiperSlide key={img}>
               <Flex
-                width={"100%"}
-                height={"100%"}
-                justifyContent={"center"}
-                alignItems={"center"}
-                position={"relative"}
+                width={'100%'}
+                height={'100%'}
+                justifyContent={'center'}
+                alignItems={'center'}
+                position={'relative'}
               >
-                <Flex position={"relative"}>
+                <Flex position={'relative'}>
                   <Image
-                    bgPosition={"center"}
-                    maxHeight={{ base: "25rem", md: "30rem" }}
+                    bgPosition={'center'}
+                    maxHeight={{ base: '25rem', md: '30rem' }}
                     src={
                       API_URL +
                       `/api/v1/section/post/page/${page?.id}/image?imageName=${img}`
@@ -61,16 +61,16 @@ export default function Page({
                     alt={`Image for page ${img}`}
                     onClick={() => handleImageClick(img)}
                     style={{
-                      cursor: "pointer",
+                      cursor: 'pointer',
                     }}
                   />
                   <Text
-                    fontSize={{ base: "5xl", md: "4xl" }}
+                    fontSize={{ base: '5xl', md: '4xl' }}
                     fontWeight={700}
-                    color={"#FFFFFF"}
+                    color={'#FFFFFF'}
                     top={10}
                     right={10}
-                    position={"absolute"}
+                    position={'absolute'}
                   >
                     {page.imageTitle}
                   </Text>
@@ -101,43 +101,43 @@ export default function Page({
             }
             alt={`Full-screen image for page ${fullScreenImage}`}
             style={{
-              maxWidth: "100%",
-              maxHeight: "100%",
-              objectFit: "contain",
+              maxWidth: '100%',
+              maxHeight: '100%',
+              objectFit: 'contain',
             }}
           />
         </Flex>
       )}
 
-      <Flex flexDir={"column"}>
+      <Flex flexDir={'column'}>
         <Flex
-          textAlign={"right"}
-          flexDir={"column"}
+          textAlign={'right'}
+          flexDir={'column'}
           padding={{ base: 5, md: 20 }}
           gap={{ base: 5, md: 10 }}
         >
           <Text
-            fontSize={"4xl"}
+            fontSize={'4xl'}
             color={data?.[0].jsonColor.primaryColor}
             fontWeight={600}
           >
             {page?.title}
           </Text>
           <Text
-            color={"#000000"}
+            color={'#000000'}
             fontWeight={500}
-            fontSize={{ base: "xs", md: "lg" }}
+            fontSize={{ base: 'xs', md: 'lg' }}
           >
             {page?.description}
           </Text>
         </Flex>
-        <Flex paddingLeft={20} gap={2} alignItems={"center"}>
+        <Flex paddingLeft={20} gap={2} alignItems={'center'}>
           <Button
             as="a"
             href={API_URL + `/api/v1/section/post/page/${page?.id}/paper`}
             target="_blank"
             download
-            variant={"outline"}
+            variant={'outline'}
             borderColor={data?.[0].jsonColor.primaryColor}
             borderWidth={2}
             padding={5}
@@ -147,7 +147,7 @@ export default function Page({
           <Button
             colorScheme={data?.[0].jsonColor.primaryColor}
             onClick={onClose}
-            variant={"outline"}
+            variant={'outline'}
             borderColor={data?.[0].jsonColor.primaryColor}
             borderWidth={2}
             padding={5}
@@ -157,5 +157,5 @@ export default function Page({
         </Flex>
       </Flex>
     </Flex>
-  );
+  )
 }
